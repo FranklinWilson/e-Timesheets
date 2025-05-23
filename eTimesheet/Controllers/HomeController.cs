23e0862a6c7bc181d2e9854280f5f434350d6d43
Frank Wilson <franklinhw@gmail.com>
2025-05-23 17:19:41 +0100

    Project setup with home, nav bar, and single role auth

---
+++ eTimesheet/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+using System.Diagnostics;
+using eTimesheet.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eTimesheet.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Timesheets()
+        {
+            return View();
+        }
+
+        public IActionResult Employees()
+        {
+            return View();
+        }
+
+        public IActionResult Jobs()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ eTimesheet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eTimesheet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
